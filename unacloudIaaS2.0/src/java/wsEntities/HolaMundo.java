735c3e164cb8b69c77f7d48ce9701c44cf58cf6d
gasotelo <gasotelo@gmail.com>
2014-01-15 08:52:10 -0500

    WS Test.

---
+++ unacloudIaaS2.0/src/java/wsEntities/HolaMundo.java
@@ -0,0 +1,11 @@
+package wsEntities;
+
+public class HolaMundo {
+	String pecuequita;
+	public HolaMundo(String pequiequita) {
+		this.pecuequita = pequiequita;
+	}
+	public String getPequiequita() {
+		return pecuequita;
+	}
+}
