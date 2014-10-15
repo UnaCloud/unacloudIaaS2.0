c80bb50390cf7482e690b5f36725681b64db8fe2
Andr‚s Romero <andresrrey@gmail.com>
2014-10-15 16:03:35 -0500

    Merge fixed

--- unacloudIaaS2.0/test/unit/back/services/AWSServiceSpec.groovy
+++ unacloudIaaS2.0/test/unit/back/services/AWSServiceSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
-@TestFor(AWSService)
+@TestFor(AWSAPIService)
 class AWSServiceSpec extends Specification {
 
     def setup() {
