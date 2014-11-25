345ff01309b0979b19c19eaba299e01a46b83457
gasotelo <gasotelo@gmail.com>
2014-11-25 12:29:21 -0500

    VirtualMachineRequest updated to have Hardware Profile instead of Cores/RAM amount

--- UnaCloudAPI/src/unacloudws/requests/VirtualImageRequest.java
+++ UnaCloudAPI/src/unacloudws/requests/VirtualImageRequest.java
@@ -1,23 +1,19 @@
 package unacloudws.requests;
 
 public class VirtualImageRequest {
-	int instances,ram,cores,imageId;
-	String hostname;
-	public VirtualImageRequest(int instances, int ram, int cores, int imageId, String hostname) {
+	int instances,imageId;
+	String hostname,hardwareProfile;
+	public VirtualImageRequest(int instances, String hardwareprofilename, int imageId, String hostname) {
 		this.instances = instances;
-		this.ram = ram;
-		this.cores = cores;
 		this.imageId = imageId;
+		this.hardwareProfile=hardwareprofilename;
 		this.hostname = hostname;
-	}
-	public int getCores() {
-		return cores;
 	}
 	public int getInstances() {
 		return instances;
 	}
-	public int getRam() {
-		return ram;
+	public String getHardwareProfile() {
+		return hardwareProfile;
 	}
 	public int getImageId() {
 		return imageId;
