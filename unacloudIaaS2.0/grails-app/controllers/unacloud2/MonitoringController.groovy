4ec3742fbf6c25ce0148696680d31e32c448db38
Cesar Forero <cesar.forero.gz@gmail.com>
2015-05-15 20:06:11 -0500

    Refactor monitoring views

--- unacloudIaaS2.0/grails-app/controllers/unacloud2/MonitoringController.groovy
+++ unacloudIaaS2.0/grails-app/controllers/unacloud2/MonitoringController.groovy
@@ -2,6 +2,8 @@ package unacloud2
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 import javassist.bytecode.stackmap.BasicBlock.Catch;
 
@@ -50,5 +52,37 @@ class MonitoringController {
 			redirect(uri:"/error", absolute:true)
 			return false
 		}
+	}
+	def getReportsLab(){
+		try{
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS aa");
+			Date init = dateFormat.parse(params.year+'-'+(params.month.length()==1?'0'+params.month:params.month)+'-'+(params.day.length()==1?'0'+params.day:params.day)+' '+params.hour+':00:00.000 '+params.sched);
+			Date end = new Date(init.getTime()+(1000*60*60*Integer.parseInt(params.range)));
+			Boolean energy = params.report[1].equals("energy")?true:false;
+			Date d = new Date();		
+			println params.lab
+			String title = (energy?"Energy":"CPU")+"Reports_lab_"+Laboratory.get(Integer.parseInt(params.lab)).name+"_"+(d.getYear()+1900)+"-"+(d.getMonth()+1)+"-"+d.getDate()+'_'+d.getHours()+"-"+d.getMinutes();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream()
+			ZipOutputStream zipFile = new ZipOutputStream(baos)
+			params.each {
+				if (it.key.contains("machine")){					
+					PhysicalMachine pm= PhysicalMachine.get((it.key - "machine") as Integer)
+					def file = monitoringService.generateReport(pm.name, init, end,energy);
+					zipFile.putNextEntry(new ZipEntry(file.getName()))		
+					file.withInputStream { i ->	zipFile << i }
+					zipFile.closeEntry()
+				}
+			}
+			zipFile.finish()
+		    response.setHeader("Content-disposition", "filename=\""+title+".zip\"")
+		    response.contentType = "application/zip"
+		    response.outputStream << baos.toByteArray()
+		    response.outputStream.flush()
+		}catch(Exception e){
+			e.printStackTrace();
+			flash.message="There is an error with file, check logs for more information"
+			redirect(uri:"/error", absolute:true)
+			return false
+		}		
 	}
 }
